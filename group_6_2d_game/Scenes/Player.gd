6f19154c4ba9ded44b72b5a5fab013cbc1e4c7dd
Mabel <ma.b.el.202423@gmail.com>
2024-10-23 22:07:15 -0400

    Fixed the walking left animation and added background music

--- group_6_2d_game/Scenes/Player.gd
+++ group_6_2d_game/Scenes/Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed: float = 60.0
+@export var speed: float = 40.0
 var direction: Vector2 = Vector2.ZERO
 
 # Reference to the Player Sprite2D node and its AnimationPlayer
@@ -13,8 +13,10 @@ func _physics_process(_delta):
 	
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
+		sprite.flip_h = false
 	elif Input.is_action_pressed("ui_left"):
 		direction.x -= 1
+		sprite.flip_h = true
 	if Input.is_action_pressed("ui_down"):
 		direction.y += 1
 	elif Input.is_action_pressed("ui_up"):
