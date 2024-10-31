054ef2736cd1f19dcb48ebf6f557f957da60157c
sshafiq1 <183101652+sshafiq1@users.noreply.github.com>
2024-10-31 08:36:18 -0400

    Drag+Drop

--- group_6_2d_game/Scenes/Minigame3/Letters/letters.gd
+++ group_6_2d_game/Scenes/Minigame3/Letters/letters.gd
@@ -1,5 +1,18 @@
 extends Area2D
 
+var selected = false
 
-func _on_body_entered(body: Node2D) -> void:
-	queue_free() 
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int):
+	if Input.is_action_just_pressed("click"):
+		selected = true
+		
+func _physics_process(delta: float):
+	if selected:
+		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
+
+func _input(event: InputEvent):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+			selected = false
