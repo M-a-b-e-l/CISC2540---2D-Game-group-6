867050fa929bec9cc0de1460ce8c6462a7da4533
sshafiq1 <183101652+sshafiq1@users.noreply.github.com>
2024-11-01 13:01:28 -0400

    Minor Adjustments

--- group_6_2d_game/Scenes/Minigame3/drop.gd
+++ group_6_2d_game/Scenes/Minigame3/drop.gd
@@ -1,7 +1,6 @@
 extends Marker2D
 
 
-	
 func select():
 	for child in get_tree().get_nodes_in_group("droppable"):
 		child.deselect()
@@ -10,14 +9,3 @@ func select():
 func deselect():
 	modulate = Color.ANTIQUE_WHITE
 	
-
-#var selected = false
-
-#func _ready():
-	#modulate = Color(Color.MEDIUM_ORCHID, 0.7)
-#
-#func _process(delta: float):
-	#if selected:
-		#visible = true
-	#else:
-		#visible = false
