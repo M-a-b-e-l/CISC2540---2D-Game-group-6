85a0b120db5a7a55cef4c74b931580dd8ba2e541
dabest822 <dembrosdoe@gmail.com>
2024-11-04 09:16:31 -0500

    fixing output debug spam

--- group_6_2d_game/Scripts/Dialogue.gd
+++ group_6_2d_game/Scripts/Dialogue.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @export var interaction_duration: float = 4.0  # Time duration for dialogue display
+@export var debug_print_interval: float = 10.0  # How often to print debug messages in seconds
 
 # Nodes
 @onready var player = get_node("../CharacterBody2D")
@@ -10,6 +11,7 @@ extends Control
 # Variables
 var bubble_sprites: Array = []
 var aliens: Array = []
+var debug_timer: float = 0.0  # Timer for debug prints
 
 func _ready() -> void:
 	# Initialize bubble sprites and hide them
@@ -21,7 +23,7 @@ func _ready() -> void:
 			print("BubbleSprite" + str(i) + " initialized and hidden.")
 		else:
 			print("WARNING: BubbleSprite" + str(i) + " not found!")
-
+			
 	# Collect all relevant alien nodes (only those with dialogue)
 	if aliens_container:
 		for i in [1, 2, 3, 4, 10, 11]:
@@ -33,39 +35,50 @@ func _ready() -> void:
 				print("WARNING: Alien" + str(i) + " not found!")
 	else:
 		print("ERROR: Aliens container not found!")
-
+		
 	if not player:
 		print("ERROR: Player node not found!")
 	elif aliens.size() == 0:
 		print("ERROR: No relevant aliens with dialogue found!")
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if not player or aliens.size() == 0:
 		return
 
+	# Update debug timer
+	debug_timer += delta
+	var should_print_debug = debug_timer >= debug_print_interval
+	
 	# Check interactions between player and relevant aliens
 	for i in range(aliens.size()):
 		var alien = aliens[i]
 		var static_body_node = alien.get_child(0)  # Assuming StaticBody2D is the first child under each Alien node
-
+		
 		if static_body_node and static_body_node is StaticBody2D:
 			var alien_collision_shape = static_body_node.get_node_or_null("CollisionShape2D")
 			if alien_collision_shape:
 				var distance = alien_collision_shape.global_position.distance_to(player.global_position)
-				print("Distance to " + alien.name + ": " + str(distance))
-
+				
+				# Only print debug if interval has passed
+				if should_print_debug:
+					print("Distance to " + alien.name + ": " + str(distance))
+				
 				if distance < 20:  # Adjusted distance for close proximity
 					if i < bubble_sprites.size() and not bubble_sprites[i].visible:
-						print("Showing dialogue for " + alien.name)
+						print("Showing dialogue for " + alien.name)  # Keep important gameplay prints
 						show_dialogue(i)
 						await hide_dialogue_after_delay(i, interaction_duration)
-					elif distance >= 20 and bubble_sprites[i].visible:
-						print("Hiding dialogue for " + alien.name)
-						hide_dialogue(i)
-			else:
+				elif distance >= 20 and bubble_sprites[i].visible:
+					print("Hiding dialogue for " + alien.name)  # Keep important gameplay prints
+					hide_dialogue(i)
+			elif should_print_debug:
 				print("WARNING: " + alien.name + " does not have a valid CollisionShape2D.")
-		else:
+		elif should_print_debug:
 			print("WARNING: " + alien.name + " does not have a valid StaticBody2D.")
+	
+	# Reset timer if interval has passed
+	if should_print_debug:
+		debug_timer = 0.0
 
 func show_dialogue(index: int) -> void:
 	if index < bubble_sprites.size():
